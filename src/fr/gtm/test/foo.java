afb032f15a21b2b7380078ccdadaeab492a17533
adminl <adminl@localhost>
2020-01-03 15:53:08 +0100

    3

--- src/fr/gtm/test/foo.java
+++ src/fr/gtm/test/foo.java
@@ -1,6 +1,12 @@
 package fr.gtm.test;
 
 public class Foo {
-
+private int valeur;
+	public int getValeur() {
+		return valeur;
+	}
 	
+	public void lancer() {
+		valeur = random.nextInt(6) + 1;
+	}
 }
